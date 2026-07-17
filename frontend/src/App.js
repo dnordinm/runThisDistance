281ee26942e61ac5947208361972f474d1df5670
david <dnordinm@gmail.com>
2026-07-16 19:53:29 -0600

    added simple input boxes and a button that will fetch the geocoordinates, swap them from lon, lat to lat, lon and plot them on the map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,76 @@ import { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Polyline } from 'react-leaflet'
 
 function App() {
-  // hardwired coords for testing
-  const routeCoords = [
-    [51.507478, -0.127965],
-    [51.507487, -0.127954],
-    [51.507502, -0.127933],
-    [51.507517, -0.127909],
-    [51.507533, -0.127872]
-  ];
+  const [startLat, setStartLat] = useState('')
+  const [startLon, setStartLon] = useState('')
+  const [endLat, setEndLat] = useState('')
+  const [endLon, setEndLon] = useState('')
+  const [desiredDist, setDesiredDist] = useState('')
+  const [routeCoords, setRouteCoords] = useState([[51.507478, -0.127965], [51.507533, -0.127872]])
+  
+  function getRoute() {  
+      fetch('http://127.0.0.1:5555/route?start_lat=51.5074&start_lon=-0.1278&end_lat=51.5155&end_lon=-0.1420')
+      .then(r => r.json())
+      .then(data => {
+        const coords = data.routes[0].geometry.coordinates;
+        const flipped = coords.map(pair => [pair[1], pair[0]]);
+        setRouteCoords(flipped)
+      })
+  }
 
   return (
-    <MapContainer center={[51.507478, -0.127965]} zoom={17} style={{ height: '500px', width: '100%' }}>
-      <TileLayer 
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution="&copy; OpenStreetMap contributors"
+    <div>
+      <input
+        type="text"
+        value={startLat}
+        onChange={e => setStartLat(e.target.value)}
+        placeholder="Starting Latitude"
       />
-      <Polyline positions={routeCoords} />
-    </MapContainer>
+      <input
+        type="text"
+        value={startLon}
+        onChange={e => setStartLon(e.target.value)}
+        placeholder="Starting Longitude"
+      />
+      <input
+        type="text"
+        value={endLat}
+        onChange={e => setEndLat(e.target.value)}
+        placeholder="Ending Latitude"
+      />
+      <input
+        type="text"
+        value={endLon}
+        onChange={e => setEndLon(e.target.value)}
+        placeholder="Ending Longitude"
+      />
+      <input
+        type="text"
+        value={desiredDist}
+        onChange={e => setDesiredDist(e.target.value)}
+        placeholder="Set desired distance"
+      />
+
+      <button onClick={getRoute}>Get Route</button>
+
+      <MapContainer center={[51.507478, -0.127965]} zoom={17} style={{ height: '500px', width: '100%' }}>
+        <TileLayer 
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+        />
+        <Polyline positions={routeCoords} />
+      </MapContainer>
+    </div>  
   )
   
   
+  // const routeCoords = [
+  //   [51.507478, -0.127965],
+  //   [51.507487, -0.127954],
+  //   [51.507502, -0.127933],
+  //   [51.507517, -0.127909],
+  //   [51.507533, -0.127872]
+  // ];
   
   // const [message, setMessage] = useState('loading')
 
